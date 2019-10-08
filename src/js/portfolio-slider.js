b8d55f3ed9a762d0d7882adf7bf1bd4a1994d75a
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-10-08 12:09:04 +0300

    адаптировал слайдер portfolio и создал отдельный файл со скриптом для него

---
+++ src/js/portfolio-slider.js
@@ -0,0 +1,23 @@
+$(function(){
+  $('.slider').slick({
+    slidesToShow: 3,
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+  });
+});
