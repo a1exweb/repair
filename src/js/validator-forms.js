002df38848941d58020e2d268e98a83df4b6f430
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-10-10 15:24:46 +0300

    Add validator forms

---
+++ src/js/validator-forms.js
@@ -0,0 +1,81 @@
+$('#brif-form').validate({
+  rules: {
+    username: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    phone: {
+      required: true
+    },
+    email: {
+      required: true,
+      email: true
+    },
+  },
+  errorClass: "invalid",
+  errorElement: "div",
+  messages: {
+    username: {
+      required: "Укажите имя",
+      minlength: jQuery.validator.format("Минимальная длинна имени 2 символа"),
+    },
+    phone: {
+      required: "Укажите телефон",
+    },
+    email: "Введите корректный email"
+  },
+},
+);
+
+$('#offer-form').validate({
+  rules: {
+    username: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    phone: {
+      required: true
+    }
+  },
+  errorClass: "invalid-offer",
+  errorElement: "div",
+  messages: {
+    username: {
+      required: "Укажите имя",
+      minlength: jQuery.validator.format("Минимальная длинна имени 2 символа"),
+    },
+    phone: {
+      required: "Укажите телефон",
+    }
+  }
+});
+
+$('#modal-form').validate({
+  rules: {
+    username: {
+      required: true,
+      minlength: 2,
+      maxlength: 15
+    },
+    phone: {
+      required: true
+    }
+  },
+  errorClass: "invalid",
+  errorElement: "div",
+  messages: {
+    username: {
+      required: "Укажите имя",
+      minlength: jQuery.validator.format("Минимальная длинна имени 2 символа"),
+    },
+    phone: {
+      required: "Укажите телефон",
+    },
+  }
+});
+
+// mask input
+
+$('input[type="tel"]').mask('+7 (999) 999-99-99');
